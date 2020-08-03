c0c8d066fe3d420f75515fa6b60db0fe6bfb3962
ricpato07 <ricpato07@gmail.com>
2020-08-03 09:19:57 -0500

    Create heroes.js

---
+++ src/data/heroes.js
@@ -0,0 +1,26 @@
+const heroes = [
+    {
+        id: 1,
+        name : 'Spiderman',
+        owner : 'Marvel'
+    },
+    {
+        id: 2,
+        name : 'Superman',
+        owner : 'DC'
+    },
+    {
+        id: 3,
+        name : 'Flash',
+        owner : 'DC'
+    },
+    {
+        id: 4,
+        name : 'Walverine',
+        owner : 'Marvel'
+    }
+]
+
+export const owners = ['DC', 'Marvel'];
+
+export default heroes;
