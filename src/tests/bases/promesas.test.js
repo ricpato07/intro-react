88c9a78cf199056f46e6da1598748636ebda7754
ricpato07 <ricpato07@gmail.com>
2020-08-05 18:38:19 -0500

    Create promesas.test.js

---
+++ src/tests/bases/promesas.test.js
@@ -0,0 +1,30 @@
+import { getHeroeByIdAsync } from "../../bases/promesas";
+import heroes from '../../data/heroes';
+
+describe('Pruebas con promesas', () => {
+    
+    test('debe retornar un heroe async', (done) => {
+        
+        const id = 1;
+
+        getHeroeByIdAsync(id)
+        .then(heroe =>{
+          expect(heroe).toBe(heroes[0]);
+          done();
+        })
+
+    })
+
+    test('obtener error si el héroe por id no existe', (done) => {
+        
+        const id = 10;
+
+        getHeroeByIdAsync(id)
+        .catch(error=>{
+            expect(error).toBe('No se encontró ningún resultado');
+            done();
+        });
+
+    });
+    
+})
