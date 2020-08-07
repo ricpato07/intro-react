0f02c73766fcd329b9ae8da31a3630af17506cf7
ricpato07 <ricpato07@gmail.com>
2020-08-07 17:35:08 -0500

    Carpeta docs

---
+++ docs/precache-manifest.66306c77b89cecc43bcd76fb0c8382d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f05ca82cc77d1e6b5ad384532c69c0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "592baf7872ba3a8f8014",
+    "url": "/static/css/main.824eb082.chunk.css"
+  },
+  {
+    "revision": "1fed5fcffd77345a81b6",
+    "url": "/static/js/2.8f3e0345.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f3e0345.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "592baf7872ba3a8f8014",
+    "url": "/static/js/main.d07bd154.chunk.js"
+  },
+  {
+    "revision": "b57b4c68968d7e3520e2",
+    "url": "/static/js/runtime-main.3a5e2f83.js"
+  }
+]);
